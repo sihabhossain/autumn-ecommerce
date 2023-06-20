8a36a2a7db7759e4181905fc2de8bcfcc13a1022
Sihab Hossain <sihabhossain380@gmail.com>
2023-06-21 00:09:10 +0600

    hero section done

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,34 @@
-import React from 'react';
+import React from "react";
+import womenImg from "../img/woman_hero.png";
+import { Link } from "react-router-dom";
 
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <section className=" h-[800px] bg-hero bg-fuchsia-100 bg-no-repeat bg-cover bg-center py-24">
+      <div className="container mx-auto flex justify-around h-full">
+        {/* text */}
+        <div className=" px-10 flex flex-col justify-center">
+          <div className="font-semibold flex items-center uppercase">
+            <div className="border-b-[4px] border-red-500">New Trend</div>
+          </div>
+          <h1 className=" text-[70px] leading-[1.1] font-light mb-4">
+            SUMMER SALE STYLISH <br />
+            <span className="font-semibold">WOMENS</span>
+          </h1>
+          <Link
+            to={"/"}
+            className="self-start uppercase font-semibold border-b-4 border-black"
+          >
+            Discover More
+          </Link>
+        </div>
+        {/* image */}
+        <div className="hidden lg:block">
+          <img src={womenImg} alt="" />
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Hero;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React, { useContext } from "react";
 import { ProductContext } from "../contexts/ProductProvider";
 import Product from "../components/Product";
+import Hero from "../components/Hero";
 
 const Home = () => {
   const { products } = useContext(ProductContext);
@@ -15,6 +16,7 @@ const Home = () => {
 
   return (
     <div>
+      <Hero></Hero>
       <section className="py-16">
         <div className="container mx-auto">
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5 gap-[30px] max-w-sm mx-auto md:max-w-none md:mx-0">
